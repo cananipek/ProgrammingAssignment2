0bd9f769534e044cd74cc5db5a7b1c90fdc8f108
Canan Ipek <canan.ipek@gmail.com>
2017-05-25 15:51:38 -0700

    First function written

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,17 @@
 ## Creates a special matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmat <- function(mat) m <<- mat
+  getmat <- function() m
+  list(set = set, get = get,
+       setmat = setmat,
+       getmat = getmat)
 }
 
 
